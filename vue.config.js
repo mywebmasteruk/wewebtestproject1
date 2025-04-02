18c790a2869ace1782ed33b3a681f257527c3a8a
myWebmaster UK (via WeWeb) <mywebmasteruk@gmail.com>
2025-04-02 18:04:29 +0400

    v14 - show both datt sources

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     assetsDir: 'public',
     publicPath: '',
     // eslint-disable-next-line no-undef
-    pages: {"index":{"entry":"src/_front/main.js","template":"public/front.html","filename":"./index.html","lang":"en","title":"","cacheVersion":13,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"\n<script src=\"https://cdn.weweb.io/weweb-runtime.js\"></script>\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://87f2db50-a21d-4ef6-afdc-1ab7f71670a5.weweb-preview.io/"},{"rel":"alternate","hreflang":"en","href":"https://87f2db50-a21d-4ef6-afdc-1ab7f71670a5.weweb-preview.io/"}],"chunks":["chunk-vendors","chunk-common","index"]}},
+    pages: {"index":{"entry":"src/_front/main.js","template":"public/front.html","filename":"./index.html","lang":"en","title":"","cacheVersion":14,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"\n<script src=\"https://cdn.weweb.io/weweb-runtime.js\"></script>\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://87f2db50-a21d-4ef6-afdc-1ab7f71670a5.weweb-preview.io/"},{"rel":"alternate","hreflang":"en","href":"https://87f2db50-a21d-4ef6-afdc-1ab7f71670a5.weweb-preview.io/"}],"chunks":["chunk-vendors","chunk-common","index"]}},
     configureWebpack: config => {
         config.module.rules.push({
             test: /\.mjs$/,
